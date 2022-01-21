74035110f8f239e81266860b002848316d8319da
bryansource <bescobar34@yahoo.com>
2022-01-21 15:15:24 -0600

    Create a general function in saved.js

--- js/saved.js
+++ js/saved.js
@@ -1,35 +1,41 @@
 //How much money you will save with a monthly contribution
 
-//Monthly contribution
-const c = 200;
+const savedMoney = (c, time, i, m) => {
+    //Monthly contribution
+    //const c = 200;
 
-const time = 20;
+    //const time = 20;
 
-//Interest Rate(i) %
-const i = 1.20;
+    //Interest Rate(i) %
+    //const i = 1.20;
 
-//Frequency(m) 12 meses
-const m = 12;
+    //Frequency(m) 12 meses (cada mes del año)
+    //const m = 12;
 
-const periods = time*m;
+    
+    const periods = time*m;
 
-//Monthly effective interest
-const lm = i/m;
+    //Monthly effective interest
+    const lm = i/m;
 
-const currentValue = (c, lm, periods) => {
-    //Update factor(uf)
-    const uf = (1-(1+(lm/100))**-periods)/(lm/100);
-    //Early rent adjustment(era)
-    const era = 1 + (lm/100);
-    return c*uf*era;
+    const currentValue = (c, lm, periods) => {
+        //Update factor(uf)
+        const uf = (1-(1+(lm/100))**-periods)/(lm/100);
+        //Early rent adjustment(era)
+        const era = 1 + (lm/100);
+        return c*uf*era;
+    }
+
+    const cValue = currentValue(c, lm, periods).toFixed(2);
+    console.log(cValue); //42698.22
+
+    //capitalization factor(cf)
+    const cf = (1+lm/100)**periods;
+
+    const finalValue = (cValue * cf).toFixed(2);
+
+    return finalValue;
 }
 
-const cValue = currentValue(c, lm, periods).toFixed(2);
-console.log(cValue); //42698.22
+//console.log(savedMoney(200, 20, 1.2, 12));
 
-//capitalization factor(cf)
-const cf = (1+lm/100)**periods;
-
-const finalValue = (cValue * cf).toFixed(2);
-
-console.log(finalValue);
